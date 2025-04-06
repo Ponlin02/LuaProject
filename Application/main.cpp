16a0a73c81715ba95d1f6bbc34abac1b0e8cdf50
Ponlin02 <ponlin.02@gmail.com>
2025-04-06 15:56:39 +0200

    Initial commit

---
+++ Application/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <thread>
+#include <string>
+#include <Windows.h>
+
+#include "lua.hpp"
+
+void DumpError(lua_State* L)
+{
+	if (lua_gettop(L) && lua_isstring(L, -1))
+	{
+		std::cout << "Lua error: " << lua_tostring(L, -1) << std::endl;
+		lua_pop(L, 1);
+	}
+}
+
+void ConsoleThreadFunction(lua_State* L)
+{
+	std::string input;
+	while (GetConsoleWindow())
+	{
+		std::cout << "> ";
+		std::getline(std::cin, input);
+
+		if (luaL_dostring(L, input.c_str()) != LUA_OK)
+		{
+			DumpError(L);
+		}
+	}
+}
+
+int main()
+{
+	lua_State* L = luaL_newstate();
+	luaL_openlibs(L);
+
+	std::thread consoleThread(ConsoleThreadFunction, L);
+
+	bool running = true;
+	while (running)
+	{
+		//Update game
+		//Render game
+	}
+	
+	return 0;
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# LuaProject
+ Projetct for learning scripting using Lua by Pontus Lindvall and Emil Halvarsson
