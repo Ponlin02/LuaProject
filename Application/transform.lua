083985a59e150c981a38e68ef496feef4b3cd78b
HalvEmil <145006109+HalvEmil@users.noreply.github.com>
2025-04-30 16:13:05 +0200

    transform.__tostring(()

--- Application/transform.lua
+++ Application/transform.lua
@@ -6,7 +6,17 @@ function transform.istransform(t)
 	return getmetatable(t) == transform
 end
 
-function transform.v1new(p, r, s)
+function transform.__newindex(t,k,v)
+	print("transform - not possible to assign new fields")
+end
+
+function transform:__tostring()
+	return "position: " .. self.position:__tostring() ..
+			", rotation :" .. self.rotation:__tostring() ..
+			", scale: " .. self.scale:__tostring()
+end
+
+function transform.new(p, r, s)
 	--assert(p == nil or vector.isvector(p), "transform new - expected 
 		--args: vector is nil")
 
