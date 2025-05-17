4237778d9085f446d7b84cf3726c23822794608e
Ponlin02 <ponlin.02@gmail.com>
2025-05-17 18:05:56 +0200

    Maze upgrade
    Testing building blocks in the maze

--- Application/gameloop/maze/maze.cpp
+++ Application/gameloop/maze/maze.cpp
@@ -4,4 +4,22 @@
 void maze::draw()
 {
 	DrawSphere(Vector3{ 0.0f, 0.0f, -15.0f }, 1.5f, RED);
+
+	//Floor test
+	Vector3 floorPosition = { 0.0f, 0.0f, 0.0f };
+	Vector3 floorSize = { 5.0f, 0.1f, 5.0f };
+
+	if (IsKeyDown(KEY_X))
+	{
+		DrawCubeWiresV(floorPosition, floorSize, BLACK);
+		DrawCubeV(floorPosition, floorSize, ORANGE);
+	}
+	
+	//Wall test
+	float height = 4.0f;
+	Vector3 wallPosition = { floorPosition.x + floorSize.x/2, height / 2, 0.0f};
+	Vector3 wallSize = { 0.4f, height, floorSize.z };
+
+	DrawCubeWiresV(wallPosition, wallSize, BLACK);
+	DrawCubeV(wallPosition, wallSize, LIME);
 }
